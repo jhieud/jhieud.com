b4a380ca42176a2b8b151be3da4491ce16be4836
jhieud <56033242+jhieud@users.noreply.github.com>
2020-11-09 19:45:27 -0500

    more styling

--- scripts/script.js
+++ scripts/script.js
@@ -6,5 +6,12 @@ function switchTabs(tab) {
   for (i = 0; i < x.length; i++) {
     x[i].style.display = "none";
   }
+
+
+  folder = document.getElementsByClassName("folder");
+  for (i = 0; i < x.length; i++) {
+    folder[i].className = folder[i].className.replace(" w3-red", "");
+  }
   document.getElementById(tab).style.display = "block";
+  evt.currentTarget.className += " w3-red";
 }
